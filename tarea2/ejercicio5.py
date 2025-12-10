a7898e144215fad3ff96f3a6f81e9381a2df7ba2
Yurani Cardona <neira.cardona@est.iudigital.edu.co>
2025-12-09 22:01:12 -0500

    Ejercicio 5 Tarea 2

---
+++ tarea2/ejercicio5.py
@@ -0,0 +1,17 @@
+def adelante(n):
+    for i in range(n):
+        print("→", end="")
+    print()
+
+def abajo(n):
+    for i in range(n):
+        print("↓")
+
+adelante(5)
+abajo(2)
+
+adelante(5)
+abajo(2)
+
+adelante(5)
+abajo(2)
